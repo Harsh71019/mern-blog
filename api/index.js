caa5342be958069b94c321305ab62adcf5030f47
Harsh Patel <harsh710patel@gmail.com>
2024-06-14 13:32:21 +0530

    setup mongodn

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODBURL)
+  .then(() => {
+    console.log('MongoDB is connected!!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
   console.log('server is running on port 3000');
 });
+
+// mongodb+srv://<username>:<password>@mern-cluster.vawf1hp.mongodb.net/

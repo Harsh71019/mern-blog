9b1e85f2e8809195a2cef30f9f8434fab40e5211
Harsh Patel <harsh710patel@gmail.com>
2024-06-14 12:41:36 +0530

    add header sections

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import { Button, Navbar, TextInput } from 'flowbite-react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon } from 'react-icons/fa';
 
 const Header = () => {
+  const path = useLocation().pathname;
   return (
     <Navbar className='border-b-2'>
       <Link
@@ -26,25 +27,26 @@ const Header = () => {
       <Button className='w-12 h-10 lg:hidden' color='gray' pill>
         <AiOutlineSearch />
       </Button>
-      <div className='flex gap-2 md:order:2'>
+      <div className='flex gap-2 md:order-2'>
         <Button className='w-12 h-10 sm:inline hidden' color='gray' pill>
           <FaMoon />
         </Button>
         <Link to='/signin'>
           <Button gradientDuoTone='purpleToBlue'>Sign In</Button>
         </Link>
-        <Navbar.Collapse>
-          <Navbar.Link>
-            <Link to='/'>Home</Link>
-          </Navbar.Link>
-          <Navbar.Link>
-            <Link to='/about'>About</Link>
-          </Navbar.Link>
-          <Navbar.Link>
-            <Link to='/projects'>Projects</Link>
-          </Navbar.Link>
-        </Navbar.Collapse>
+        <Navbar.Toggle />
       </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === '/'} as={'div'}>
+          <Link to='/'>Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/about'} as={'div'}>
+          <Link to='/about'>About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/projects'} as={'div'}>
+          <Link to='/projects'>Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 };

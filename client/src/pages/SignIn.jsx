166251733b52e4f9bb825143c3728a3d4cc0551d
Harsh Patel <harsh710patel@gmail.com>
2024-06-15 18:41:16 +0530

    complete sign in

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,5 +1,122 @@
-const SignIn = () => {
-  return <div>SignIn</div>;
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all fields');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if (data.success === false) {
+        setErrorMessage(data.message);
+      }
+
+      if (res.ok) {
+        navigate('/');
+      }
+
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      if (error) {
+        return setErrorMessage('Something went wrong please try again!');
+      }
+    }
+  };
+
+  return (
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        <div className='flex-1'>
+          {/* Left */}
+          <Link to='/' className='font-bold dark:text-white text-4xl'>
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white '>
+              Harsh's
+            </span>{' '}
+            Blog
+          </Link>
+          <p className='text-sm mt-5'>
+            This is a demo project. You can sign in with email and password or
+            with Google.
+          </p>
+        </div>
+        <div className='flex-1'>
+          {/* right */}
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+            <div className=''>
+              <Label value='Your email'></Label>
+              <TextInput
+                type='email'
+                placeholder='name@company.com'
+                id='email'
+                onChange={handleChange}
+              />
+            </div>{' '}
+            <div className=''>
+              <Label value='Your password'></Label>
+              <TextInput
+                type='password'
+                placeholder='************'
+                id='password'
+                onChange={handleChange}
+              />
+            </div>
+            <Button
+              gradientDuoTone='purpleToPink'
+              type='submit'
+              disabled={loading}
+            >
+              {loading ? (
+                <Spinner size='sm'>
+                  <span>Loading...</span>
+                </Spinner>
+              ) : (
+                'Sign In'
+              )}
+            </Button>
+          </form>
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>Don't Have a account ?</span>
+            <Link className='text-blue-500' to='/signup'>
+              Sign Up
+            </Link>
+          </div>
+          {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default SignIn;
+export default SignUp;
